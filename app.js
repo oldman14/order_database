7e67c4c227df0d707fcd8a105c42b65a782a882f
HoangThai <thaibuaplol@gmail.com>
2021-09-02 14:07:45 +0700

    Update PORT in app.js

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ mongooso.connect(
   }
 );
 
-app.listen(3000);
+// app.listen(3000);
 //open server at port 3000
-// app.listen(process.env.PORT || 3000, ()=>{
-//     console.log("Server is listening on port 3000");
-// })
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening on port 3000");
+});
